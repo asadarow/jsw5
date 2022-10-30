de444047dfab03987ed1c279c1325e15c127104a
jiji <asadarow@gmail.com>
2022-10-30 14:05:34 +0800

    Update 2022-10-30T06:05:31.692Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -28,30 +28,43 @@ var data = [{
   "price": 1765,
   "rate": 7
 }];
-var list = document.querySelector(".result-list"); // 搜尋結果
+var list = document.querySelector(".result-list");
+var result = document.querySelector(".search-result");
+var resultlength = 0; // 搜尋結果
 
 function init() {
   var str = "";
+  var resultstr = "";
   data.forEach(function (item) {
-    str += "<li class=\"card\">\n    <div class=\"cover\">\n      <p class=\"area\">".concat(item.area, "</p>\n      <p class=\"score\">").concat(item.rate, "</p>\n      <img src=").concat(item.imgUrl, " alt=\"#\">\n    </div>\n    <div class=\"content\">\n      <div class=\"depiction\">\n        <h2>").concat(item.name, "</h2>\n        <p>").concat(item.description, "</p>\n      </div>\n      <div class=\"purchase\">\n        <p>\n          <span class=\"material-icons\">\n          error\n          </span>\n          \u5269\u4E0B\u6700\u5F8C ").concat(item.group, " \u7D44</p>\n        <h3><span>TWD</span>$").concat(item.price, "</h3>\n      </div>\n    </div>\n    </li>");
+    str += "<li class=\"card\"><div class=\"cover\"><p class=\"area\">".concat(item.area, "</p><p class=\"score\">").concat(item.rate, "</p><a href=\"#\"><img src=").concat(item.imgUrl, " alt=\"#\"></a></div><div class=\"content\"><div class=\"depiction\"><h2>").concat(item.name, "</h2><p>").concat(item.description, "</p></div><div class=\"purchase\"><p><span class=\"material-icons\">error</span>\u5269\u4E0B\u6700\u5F8C ").concat(item.group, " \u7D44</p><h3><span>TWD</span>$").concat(item.price, "</h3></div></div></li>");
     list.innerHTML = str;
   });
+  resultlength = data.length;
+  resultstr += "\u672C\u6B21\u641C\u5C0B\u5171 ".concat(resultlength, " \u7B46\u8CC7\u6599");
+  result.innerHTML = resultstr;
 }
 
-; // 篩選條件
+;
+init(); // 篩選條件
 
 var filter = document.querySelector(".filter");
 filter.addEventListener("click", function (e) {
   var str = "";
+  var resultstr = "";
+  resultlength = 0;
   data.forEach(function (item) {
     if (e.target.value == "全部地區") {
-      str += "<li class=\"card\">\n    <div class=\"cover\">\n      <p class=\"area\">".concat(item.area, "</p>\n      <p class=\"score\">").concat(item.rate, "</p>\n      <img src=").concat(item.imgUrl, " alt=\"#\">\n    </div>\n    <div class=\"content\">\n      <div class=\"depiction\">\n        <h2>").concat(item.name, "</h2>\n        <p>").concat(item.description, "</p>\n      </div>\n      <div class=\"purchase\">\n        <p>\n          <span class=\"material-icons\">\n          error\n          </span>\n          \u5269\u4E0B\u6700\u5F8C ").concat(item.group, " \u7D44</p>\n        <h3><span>TWD</span>$").concat(item.price, "</h3>\n      </div>\n    </div>\n    </li>");
+      str += "<li class=\"card\"><div class=\"cover\"><p class=\"area\">".concat(item.area, "</p><p class=\"score\">").concat(item.rate, "</p><a href=\"#\"><img src=").concat(item.imgUrl, " alt=\"#\"></a></div><div class=\"content\"><div class=\"depiction\"><h2>").concat(item.name, "</h2><p>").concat(item.description, "</p></div><div class=\"purchase\"><p><span class=\"material-icons\">error</span>\u5269\u4E0B\u6700\u5F8C ").concat(item.group, " \u7D44</p><h3><span>TWD</span>$").concat(item.price, "</h3></div></div></li>");
+      resultlength = data.length;
     } else if (e.target.value == item.area) {
-      str += "<li class=\"card\">\n      <div class=\"cover\">\n        <p class=\"area\">".concat(item.area, "</p>\n        <p class=\"score\">").concat(item.rate, "</p>\n        <img src=").concat(item.imgUrl, " alt=\"#\">\n      </div>\n      <div class=\"content\">\n        <div class=\"depiction\">\n          <h2>").concat(item.name, "</h2>\n          <p>").concat(item.description, "</p>\n        </div>\n        <div class=\"purchase\">\n          <p>\n            <span class=\"material-icons\">\n            error\n            </span>\n            \u5269\u4E0B\u6700\u5F8C ").concat(item.group, " \u7D44</p>\n          <h3><span>TWD</span>$").concat(item.price, "</h3>\n        </div>\n      </div>\n      </li>");
+      str += "<li class=\"card\"><div class=\"cover\"><p class=\"area\">".concat(item.area, "</p><p class=\"score\">").concat(item.rate, "</p><a href=\"#\"><img src=").concat(item.imgUrl, " alt=\"#\"></a></div><div class=\"content\"><div class=\"depiction\"><h2>").concat(item.name, "</h2><p>").concat(item.description, "</p></div><div class=\"purchase\"><p><span class=\"material-icons\">error</span>\u5269\u4E0B\u6700\u5F8C ").concat(item.group, " \u7D44</p><h3><span>TWD</span>$").concat(item.price, "</h3></div></div></li>");
+      resultlength++;
     }
 
     list.innerHTML = str;
   });
+  resultstr += "\u672C\u6B21\u641C\u5C0B\u5171 ".concat(resultlength, " \u7B46\u8CC7\u6599");
+  result.innerHTML = resultstr;
 }); // 新增資料
 
 var btn = document.querySelector(".btn");
@@ -63,21 +76,42 @@ var count = document.querySelector("#count");
 var star = document.querySelector("#star");
 var memo = document.querySelector("#memo");
 btn.addEventListener("click", function (e) {
-  var obj = {};
-  obj.name = fname.value;
-  obj.imgUrl = imgurl.value;
-  obj.area = area.value;
-  obj.description = memo.value;
-  obj.group = count.value;
-  obj.price = price.value;
-  obj.rate = star.value;
-  data.push(obj);
-  init(); // fname.value="";
-  // imgurl.value="";
-  // area.value="";
-  // memo.value="";
-  // count.value="";
-  // price.value="";
-  // star.value="";
+  if (fname.value.length < 1) {
+    alert("請填寫套票名稱");
+  } else if (imgurl.value.length <= 1) {
+    alert("請填寫圖片網址");
+  } else if (area.value === "") {
+    alert("請選擇景點地區");
+  } else if (price.value < 1) {
+    alert("套票金額請填寫大於1的數字");
+  } else if (count.value < 1) {
+    alert("套票組數請填寫大於1的數字");
+  } else if (star.value < 1 || star.value > 10) {
+    alert("套票星級請填寫1到10之間的數字");
+  } else if (memo.value.length < 1) {
+    alert("請填寫套票描述");
+  } else {
+    var obj = {};
+    obj.name = fname.value;
+    obj.imgUrl = imgurl.value;
+    obj.area = area.value;
+    obj.description = memo.value;
+    obj.group = count.value;
+    obj.price = price.value;
+    obj.rate = star.value;
+    data.push(obj);
+    resultlength++;
+    alert("新增成功!");
+    init();
+    console.log(data);
+    fname.value = "";
+    imgurl.value = "";
+    area.value = "";
+    memo.value = "";
+    count.value = "";
+    price.value = "";
+    star.value = "";
+    filter.value = "全部地區";
+  }
 });
 //# sourceMappingURL=all.js.map
