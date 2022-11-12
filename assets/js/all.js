72e2746d60c9155ffa50ea291ef8f52a9e2cc052
jiji <asadarow@gmail.com>
2022-11-13 01:30:26 +0800

    Update 2022-11-12T17:30:23.680Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,78 +1,64 @@
 "use strict";
 
-// let data = [
-//   {
-//     "id": 0,
-//     "name": "肥宅心碎賞櫻3日",
-//     "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-//     "area": "高雄",
-//     "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-//     "group": 87,
-//     "price": 1400,
-//     "rate": 10
-//   },
-//   {
-//     "id": 1,
-//     "name": "貓空纜車雙程票",
-//     "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-//     "area": "台北",
-//     "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-//     "group": 99,
-//     "price": 240,
-//     "rate": 2
-//   },
-//   {
-//     "id": 2,
-//     "name": "台中谷關溫泉會1日",
-//     "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-//     "area": "台中",
-//     "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-//     "group": 20,
-//     "price": 1765,
-//     "rate": 7
-//   }
-// ];
-var data = [];
+var data = []; // 搜尋結果
+
 axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json').then(function (response) {
   data = response.data.data;
-  init();
+  renderCard(data);
+  renderC3(data);
 });
 var list = document.querySelector(".result-list");
 var result = document.querySelector(".search-result");
-var resultlength = 0; // 搜尋結果
+var resultlength = 0; // 更新圖表
 
-function init() {
-  var str = "";
-  var resultstr = "";
+function renderC3(data) {
+  var newData = [];
+  var totalObj = {};
   data.forEach(function (item) {
-    str += "<li class=\"card\"><div class=\"cover\"><p class=\"area\">".concat(item.area, "</p><p class=\"score\">").concat(item.rate, "</p><a href=\"#\"><img src=").concat(item.imgUrl, " alt=\"#\"></a></div><div class=\"content\"><div class=\"depiction\"><h2>").concat(item.name, "</h2><p>").concat(item.description, "</p></div><div class=\"purchase\"><p><span class=\"material-icons\">error</span>\u5269\u4E0B\u6700\u5F8C ").concat(item.group, " \u7D44</p><h3><span>TWD</span>$").concat(item.price, "</h3></div></div></li>");
-    list.innerHTML = str;
+    if (totalObj[item.area] == undefined) {
+      totalObj[item.area] = 1;
+    } else {
+      totalObj[item.area]++;
+    }
   });
-  resultlength = data.length;
-  resultstr += "\u672C\u6B21\u641C\u5C0B\u5171 ".concat(resultlength, " \u7B46\u8CC7\u6599");
-  result.innerHTML = resultstr;
+  Object.keys(totalObj).forEach(function (item) {
+    var ary = [];
+    ary.push(item);
+    ary.push(totalObj[item]);
+    newData.push(ary);
+  });
+  var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: newData,
+      type: "donut",
+      colors: {
+        "高雄": "#E68618",
+        "台中": "#5151D3",
+        "台北": "#26BFC7"
+      }
+    },
+    donut: {
+      title: "套票地區比重"
+    }
+  });
 }
 
 ; // 篩選條件
 
 var filter = document.querySelector(".filter");
 filter.addEventListener("click", function (e) {
-  var str = "";
-  var resultstr = "";
-  resultlength = 0;
   data.forEach(function (item) {
     if (e.target.value == "全部地區") {
-      str += "<li class=\"card\"><div class=\"cover\"><p class=\"area\">".concat(item.area, "</p><p class=\"score\">").concat(item.rate, "</p><a href=\"#\"><img src=").concat(item.imgUrl, " alt=\"#\"></a></div><div class=\"content\"><div class=\"depiction\"><h2>").concat(item.name, "</h2><p>").concat(item.description, "</p></div><div class=\"purchase\"><p><span class=\"material-icons\">error</span>\u5269\u4E0B\u6700\u5F8C ").concat(item.group, " \u7D44</p><h3><span>TWD</span>$").concat(item.price, "</h3></div></div></li>");
-      resultlength = data.length;
+      renderCard(data);
     } else if (e.target.value == item.area) {
-      str += "<li class=\"card\"><div class=\"cover\"><p class=\"area\">".concat(item.area, "</p><p class=\"score\">").concat(item.rate, "</p><a href=\"#\"><img src=").concat(item.imgUrl, " alt=\"#\"></a></div><div class=\"content\"><div class=\"depiction\"><h2>").concat(item.name, "</h2><p>").concat(item.description, "</p></div><div class=\"purchase\"><p><span class=\"material-icons\">error</span>\u5269\u4E0B\u6700\u5F8C ").concat(item.group, " \u7D44</p><h3><span>TWD</span>$").concat(item.price, "</h3></div></div></li>");
-      resultlength++;
+      var tempData = [];
+      data.forEach(function (item) {
+        if (e.target.value == item.area) tempData.push(item);
+      });
+      renderCard(tempData);
     }
-
-    list.innerHTML = str;
   });
-  resultstr += "\u672C\u6B21\u641C\u5C0B\u5171 ".concat(resultlength, " \u7B46\u8CC7\u6599");
-  result.innerHTML = resultstr;
 }); // 新增資料
 
 var btn = document.querySelector(".btn");
@@ -112,9 +98,22 @@ btn.addEventListener("click", function (e) {
     data.push(obj);
     resultlength++;
     alert("新增成功!");
-    init();
+    renderCard(data);
+    renderC3(data);
     filter.value = "全部地區";
     addform.reset();
   }
-});
+}); // 渲染卡片
+
+function renderCard(renderData) {
+  var str = "";
+  var resultstr = "";
+  renderData.forEach(function (item) {
+    str += "<li class=\"card\"><div class=\"cover\"><p class=\"area\">".concat(item.area, "</p><p class=\"score\">").concat(item.rate, "</p><a href=\"#\"><img src=").concat(item.imgUrl, " alt=\"#\"></a></div><div class=\"content\"><div class=\"depiction\"><h2>").concat(item.name, "</h2><p>").concat(item.description, "</p></div><div class=\"purchase\"><p><span class=\"material-icons\">error</span>\u5269\u4E0B\u6700\u5F8C ").concat(item.group, " \u7D44</p><h3><span>TWD</span>$").concat(item.price, "</h3></div></div></li>");
+    list.innerHTML = str;
+  });
+  resultlength = renderData.length;
+  resultstr += "\u672C\u6B21\u641C\u5C0B\u5171 ".concat(resultlength, " \u7B46\u8CC7\u6599");
+  result.innerHTML = resultstr;
+}
 //# sourceMappingURL=all.js.map
